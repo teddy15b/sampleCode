74e7e232b49dcf71d9dad3dbc3c805cac7a34646
teddy <teddy15b@gmail.com>
2015-08-25 12:15:42 +0800

    remove unused code and merge from yugzan

--- springSecurity-boot-gradle-mongodb/src/main/java/spring/security/boot/mongodb/domain/Account.java
+++ springSecurity-boot-gradle-mongodb/src/main/java/spring/security/boot/mongodb/domain/Account.java
@@ -19,103 +19,98 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  */
 @Document(collection = "users")
 public class Account implements UserDetails, CredentialsContainer {
+  private static final long serialVersionUID = -2486911392546018078L;
+  public static final String USERNAME = "username";
+  public static final String PASSWORD = "password";
+  public static final String ROLES = "roles";
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2486911392546018078L;
-	public static final String USERNAME = "username";
-	public static final String PASSWORD = "password";
-	public static final String ROLES = "roles";
+  private String username;
+  private String password;
+  private boolean accountNonExpired;
+  private boolean accountNonLocked;
+  private boolean credentialsNonExpired;
+  private boolean enabled;
 
-	private String username;
-	private String password;
-	private boolean accountNonExpired;
-	private boolean accountNonLocked;
-	private boolean credentialsNonExpired;
-	private boolean enabled;
+  private List<String> roles;
 
-	private List<String> roles;
+  public String getUsername() {
+    return username;
+  }
 
-	public String getUsername() {
-		return username;
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setRoles(List<String> roles) {
+    this.roles = roles;
+  }
 
-	public void setRoles(List<String> roles) {
-		this.roles = roles;
-	}
+  public List<String> getRoles() {
+    return roles;
+  }
 
-	public List<String> getRoles() {
-		return roles;
-	}
+  public boolean isAccountNonExpired() {
+    return accountNonExpired;
+  }
 
-	public boolean isAccountNonExpired() {
-		return accountNonExpired;
-	}
+  public void setAccountNonExpired(boolean accountNonExpired) {
+    this.accountNonExpired = accountNonExpired;
+  }
 
-	public void setAccountNonExpired(boolean accountNonExpired) {
-		this.accountNonExpired = accountNonExpired;
-	}
+  public boolean isAccountNonLocked() {
+    return accountNonLocked;
+  }
 
-	public boolean isAccountNonLocked() {
-		return accountNonLocked;
-	}
+  public void setAccountNonLocked(boolean accountNonLocked) {
+    this.accountNonLocked = accountNonLocked;
+  }
 
-	public void setAccountNonLocked(boolean accountNonLocked) {
-		this.accountNonLocked = accountNonLocked;
-	}
+  public boolean isCredentialsNonExpired() {
+    return credentialsNonExpired;
+  }
 
-	public boolean isCredentialsNonExpired() {
-		return credentialsNonExpired;
-	}
+  public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+    this.credentialsNonExpired = credentialsNonExpired;
+  }
 
-	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
-		this.credentialsNonExpired = credentialsNonExpired;
-	}
+  public boolean isEnabled() {
+    return enabled;
+  }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this).append("username", username).append("password", password)
+        .append("roles", roles).append("accountNonExpired", accountNonExpired)
+        .append("accountNonLocked", accountNonLocked)
+        .append("credentialsNonExpired", credentialsNonExpired).append("enabled", enabled)
+        .toString();
+  }
 
-	@Override
-	public String toString() {
-		return new ToStringBuilder(this).append("username", username)
-				.append("password", password).append("roles", roles)
-				.append("accountNonExpired", accountNonExpired)
-				.append("accountNonLocked", accountNonLocked)
-				.append("credentialsNonExpired", credentialsNonExpired)
-				.append("enabled", enabled).toString();
-	}
-	
-	@JsonIgnore
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Collection<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-		roles.forEach(role -> {
-			authorities.add(new SimpleGrantedAuthority(role.toString()));
-		});
-		return authorities;
-	}
+  @JsonIgnore
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    Collection<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+    roles.forEach(role -> {
+      authorities.add(new SimpleGrantedAuthority(role.toString()));
+    });
+    return authorities;
+  }
 
-	@Override
-	public void eraseCredentials() {
-		password = null;
-	}
+  @Override
+  public void eraseCredentials() {
+    password = null;
+  }
 
 }
